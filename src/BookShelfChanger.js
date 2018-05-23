f9e4897709f82b891873134bf04114578e07d089
Pritchard Family <pritchardFamily@Pritchards-iMac.local>
2018-05-22 22:51:26 -0700

    The start of the BookShelfChanger component

---
+++ src/BookShelfChanger.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BookShelfChanger extends React.Component {
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+export default BookShelfChanger;
