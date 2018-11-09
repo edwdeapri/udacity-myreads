6c7bfd5a1041b1d994a50550b0225ea621bafb53
Edward Pritchard <edward.d.pritchard@gmail.com>
2018-11-09 10:02:43 -0700

    deploy: updates

--- src/BookMove.js
+++ src/BookMove.js
@@ -3,42 +3,39 @@ import PropTypes from "prop-types";
 
 /* This component creates the dropdown menu to move books to and between shelves. */
 
-class BookMove extends React.Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    books: PropTypes.array.isRequired,
-    bookMove: PropTypes.func.isRequired
-  };
+const BookMove = props => {
+  const { book, books, bookMove } = props;
+  let bookShelf = "none";
 
-  render() {
-    const { book, books, bookMove } = this.props;
-
-    let bookShelf = "none";
-
-    for (let item of books) {
-      if (item.id === book.id) {
-        bookShelf = item.shelf;
-        break;
-      }
+  for (let item of books) {
+    if (item.id === book.id) {
+      bookShelf = item.shelf;
+      break;
     }
-
-    return (
-      <div className="book-shelf-changer">
-        <select
-          onChange={event => bookMove(book, event.target.value)}
-          defaultValue={bookShelf}
-        >
-          <option value="none" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Looking Forward to Reading</option>
-          <option value="read">Already Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
   }
-}
+
+  return (
+    <div className="book-shelf-changer">
+      <select
+        onChange={event => bookMove(book, event.target.value)}
+        defaultValue={bookShelf}
+      >
+        <option value="none" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Looking Forward to Reading</option>
+        <option value="read">Already Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+};
+
+BookMove.propTypes = {
+  book: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired,
+  bookMove: PropTypes.func.isRequired
+};
 
 export default BookMove;
